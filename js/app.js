22e24b2a62de7bbf7f1e6c3b37f51e762626a06c
Francisco Jonathan Vasconcelos Monteiro <jonathan2011monteiro@hotmail.com>
2018-03-02 08:16:42 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 var form = document.getElementById("formulario");
 var campoArquivo = document.getElementById("arquivo");
 var uploadBnt = document.getElementById("enviar");
+var barra = document.getElementById("progresso");
 form.onsubmit = function(e){ //"e" vai armazenar o ato do submit
 	e.preventDefault(); //vai previnir que o fomulario envie os arquivos de maneira direta
 	uploadBnt.value = "Enviando...";
@@ -12,6 +13,18 @@ form.onsubmit = function(e){ //"e" vai armazenar o ato do submit
 	}
 	var ajax = iniciarAjax();
 	ajax.open("POST", "processa.php", true); // estar preparando o arquivo para o processa.php
+	ajax.upload.onloadstart = function(){ //vai trabalhar o inicio do arquivo
+		barra.value = 0; //inicio da barra sempre 0
+	};
+	ajax.upload.onloadprogress = function(e){ //a cada byte enviado sera executado
+		if (e.lengthComputable) { // se o tamanho for calculado
+			barra.max = e.total; //o maximo da barra recebe o valor total
+			barra.value = e.loaded; //a barra recebe o envio de envio
+		}
+	};
+	ajax.upload.onloadend = function(e){ // quando enviar
+		barra.value = e.loaded; // para a barra receber o 100%
+	};
 	ajax.send(formData);//vai enviar o arquivo para o processa.php via post em forma assíncrona
 	ajax.onload = function () {
 		if(ajax.status === 200){ // se o meu status me retornar tudo certo
